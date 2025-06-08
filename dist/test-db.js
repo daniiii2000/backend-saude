51dae8676fc4220a9ecf0b0193c6223336cd74a3
santiago noguera <santiagonoguera2225@gmail.com>
2025-06-08 14:00:59 -0300

    prisma install

--- dist/test-db.js
+++ dist/test-db.js
@@ -3,13 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 async function main() {
-    const pacientes = await prisma.paciente.findMany();
-    console.log('📋 Pacientes encontrados:', pacientes);
     const profissionais = await prisma.profissional.findMany();
-    console.log('📋 Profissionais encontrados:', profissionais);
+    console.log('Profissionais encontrados:', profissionais);
 }
 main()
     .catch((e) => {
-    console.error('❌ Erro ao conectar ou consultar:', e);
+    console.error('Erro ao conectar:', e);
 })
     .finally(() => prisma.$disconnect());
--- src/test-db.ts
+++ src/test-db.ts
@@ -3,15 +3,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const pacientes = await prisma.paciente.findMany();
-  console.log('📋 Pacientes encontrados:', pacientes);
-
   const profissionais = await prisma.profissional.findMany();
-  console.log('📋 Profissionais encontrados:', profissionais);
+  console.log('Profissionais encontrados:', profissionais);
 }
 
 main()
   .catch((e) => {
-    console.error('❌ Erro ao conectar ou consultar:', e);
+    console.error('Erro ao conectar:', e);
   })
   .finally(() => prisma.$disconnect());
